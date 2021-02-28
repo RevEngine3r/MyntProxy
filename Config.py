36840eb5c2567e68f7c10a299c638c51c9a76189
Saeed Shirvani Bagheri <hbpubgm@gmail.com>
2021-02-28 23:09:40 +0330

    Delete Config.py

--- Config.py
+++
@@ -1,9 +0,0 @@
-session_name = '9903808554'
-api_id = 1266225
-api_hash = 'ad2720e909f76e6bd78393e1177ded4e'
-
-in_id = -1001346727604
-msg_filter_str = 'port=1023'
-
-enable_proxy = False
-proxy = ('94.130.182.184', 1023, 'dd8b2394ad2ecef7e607d5f52b31a45768')
